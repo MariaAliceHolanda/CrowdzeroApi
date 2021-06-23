6a6c28e7282dd484dc88bf67cf1a021df1042d28
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-23 19:57:17 +0100

    Update AssociadoRoute.js

--- src/routes/AssociadoRoute.js
+++ src/routes/AssociadoRoute.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 //importar os controladores
 const associadoController = require('../controllers/AssociadoController')
+
+router.get('/getAssociados/:id',associadoController.getAssociados)
 module.exports = router;
