e7bdb7c3209b2e87ae75d82e51ac1d54330bfd6e
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-23 20:11:50 +0100

    Update AssociadoController.js

--- src/controllers/AssociadoController.js
+++ src/controllers/AssociadoController.js
@@ -43,7 +43,7 @@ controller.list = async (req, res) => {
 controller.getAssociados  = async (req, res) => {
   const {instituiçõeId} = req.params;
   const data = await Associados.findAll(
-    
+    {include: [Utilizador]}
   )
   .then(function(data){
   return data;
