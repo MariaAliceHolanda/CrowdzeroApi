90450191716d0d08b5b51d21edb2c6a64bfd3554
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-23 19:41:42 +0100

    Update AlertaRoute.js

--- src/routes/AlertaRoute.js
+++ src/routes/AlertaRoute.js
@@ -5,6 +5,5 @@ const router = express.Router();
 
 //importar os controladores
 const alertaController = require('../controllers/AlertaController')
-router.get('/teste',alertaController.teste);
 router.get('/list/:id',alertaController.getAlertas);
 module.exports = router;
