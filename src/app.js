3219fafe7c40f655985a5ec26b0de414c4d4187a
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-23 15:31:34 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ app.set('port', process.env.PORT|| 3000);
 //Middlewares
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type,Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+    }); 
+
+
 // Importação de Rotas
 const adminRouters = require('./routes/AdminRoute')
 const utilizadorRouters = require('./routes/UtilizadorRoute')
