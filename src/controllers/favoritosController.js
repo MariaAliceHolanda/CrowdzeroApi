c44f7bb0781620fdf8b448c019208f4e796e0a55
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-23 16:51:32 +0100

    Update favoritosController.js

--- src/controllers/favoritosController.js
+++ src/controllers/favoritosController.js
@@ -36,6 +36,6 @@ controller.list = async (req, res) => {
     .catch(error => {
     return error;
     });
-    res.json({success : true, data : data});
+    res.json({success : false, data : data});
 }
 module.exports = controller
