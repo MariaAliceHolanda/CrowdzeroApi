2da5cbb789b72a19e344325f982d44be37c228ec
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-23 09:56:02 +0100

    Update ReporteController.js

--- src/controllers/ReporteController.js
+++ src/controllers/ReporteController.js
@@ -1,15 +1,39 @@
 var Reporte = require('../model/reportes');
+var Utilizador = require('../model/utilizadores')
 var sequelize = require('../model/database');
+const { now } = require('sequelize/types/lib/utils');
 const controller = {}
 sequelize.sync()
 
-controller.teste = (req,res) => {
-    const data = {
-    name: "Nuno Costa",
-    age: 42,
-    city: 'Viseu'
+controller.create = (req,res) => {
+    
+    try {
+        // data
+    const {nivelReporte,locaiId, utilizadoreId}= req.body;
+    // create
+    const data = await Reporte.create({
+     NivelReporte: nivelReporte,
+     locaiId:locaiId,
+     utilizadoreId: utilizadoreId,
+     HoraReporte: now()
+    })
+    
+    //update reportesFeitos attribute
+    Utilizador.updateAttributes(
+        {reportesFeitos: Sequelize.literal('reportesFeitos + 1')},
+        {where: utilizadoreId}
+    )
+
+    //update reportesFeitos attribute
+    Utilizador.updateAttributes(
+        {Pontuação: Sequelize.literal('Pontuação + 1')},
+        {where: utilizadoreId}
+    )
+  
+    return res.status(200).json(data)
+        
+    } catch (error) {
+        return res.status(500).json(error)
     }
-    console.log("Envio de dados do Controlador EMPLOYEE.");
-    res.json(data);
 };
 module.exports = controller
