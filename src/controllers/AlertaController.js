f3105afd58b0a0303bf04ab4d93c9c881f31f207
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-23 19:12:58 +0100

    Update AlertaController.js

--- src/controllers/AlertaController.js
+++ src/controllers/AlertaController.js
@@ -4,16 +4,6 @@ var Local = require('../model/locais')
 const controller = {}
 sequelize.sync()
 
-controller.teste = (req,res) => {
-    const data = {
-    name: "Nuno Costa",
-    age: 42,
-    city: 'Viseu'
-    }
-    console.log("Envio de dados do Controlador EMPLOYEE.");
-    res.json(data);
-};
-
 controller.create = async (req,res) => {
     // data
     const divisão = "teste"
