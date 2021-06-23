c8c59da90d0ddd847c3415e719c5ce8e95e9cb63
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-23 11:28:35 +0100

    Update LocalRoute.js

--- src/routes/LocalRoute.js
+++ src/routes/LocalRoute.js
@@ -7,4 +7,5 @@ router.post('/create',localController.create);
 router.get('/get/:id',localController.get);
 router.get('/list/:id',localController.list);
 router.get('/statusLocal/:id',localController.setStatusLocal);
+router.get('/getQuantidadeReportes/:id'/localController.getQuantidadeReportes)
 module.exports = router;
