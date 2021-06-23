d78085c8b9a9dc3b5e009f57adf501ba25969f60
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-23 16:36:41 +0100

    Update favoritosRoute.js

--- src/routes/favoritosRoute.js
+++ src/routes/favoritosRoute.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 //importar os controladores
 const favoritosController = require('../controllers/favoritosController')
+router.post('/create',favoritosController.create)
+router.get('/list/:id',favoritosController.list)
 module.exports = router;
