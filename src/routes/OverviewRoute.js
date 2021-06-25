4262f620fb1f94b62786bda6fef375468093e164
eduardaaragao <eduardaaragao18@gmail.com>
2021-06-25 08:08:53 +0100

    rota autorizacao, insercao instituicao

--- src/routes/OverviewRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-const overviewController = require('../controllers/overviewController.js')
-router.get('/getDadosOverview/:id', overviewController.getOverviewData)
-module.exports = router;
