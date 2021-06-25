467b3c83431ff230663e79b81251540d35aad24c
eduardaaragao <eduardaaragao18@gmail.com>
2021-06-25 21:24:30 +0100

    vai no post msm

--- src/routes/AutenticaçãoRoute.js
+++ src/routes/AutenticaçãoRoute.js
@@ -12,7 +12,7 @@ router.post('/loginAdmin',authAdminController.login);
 
 router.post('/registoGestor',authGestorController.register);
 router.post('/loginGestor',authGestorController.login);
-router.get('/getGestor', middleware.checkToken, authGestorController.getGestor); //Todos os gets têm que ter checagem do token
+router.post('/getGestor', middleware.checkToken, authGestorController.getGestor); //Todos os gets têm que ter checagem do token
 
 router.post('/registoUtilizador',authUtilizadorController.register);
 router.post('/loginUtilizador',authUtilizadorController.login);
