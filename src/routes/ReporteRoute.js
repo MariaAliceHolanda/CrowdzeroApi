93cbc288723ade9d8efa7d9b96c1d179eaa47843
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-27 20:13:31 +0100

    Update ReporteRoute.js

--- src/routes/ReporteRoute.js
+++ src/routes/ReporteRoute.js
@@ -4,5 +4,5 @@ const router = express.Router();
 //importar os controladores
 const reporteController = require('../controllers/ReporteController')
 router.post('/create',reporteController.create);
-router.post('UpdatePontuacao',reporteController.UpdatePontuacao);
+router.get('UpdatePontuacao/:id',reporteController.UpdatePontuacao);
 module.exports = router;
