1c6136b08b270ce453441ecb61a421333da4257d
eduardaaragao <eduardaaragao18@gmail.com>
2021-06-27 04:56:38 +0100

    login utilizador

--- src/controllers/AuthUtilizadorController.js
+++ src/controllers/AuthUtilizadorController.js
@@ -9,8 +9,8 @@ sequelize.sync()
 controllers.register = async (req,res) => {
     const {nome, email, password} = req.body
     console.log(req.body)
-        const data = await Associados.create({
-            nome_user: nome,
+    const data = await Associados.create({
+        nome_user: nome,
             email_user: email,
             password_user: password
         }).then(function(data){
@@ -27,35 +27,36 @@ controllers.register = async (req,res) => {
 }
 
 controllers.login = async (req,res) => {
-    if (req.body.email && req.body.password) {
-    var email = req.body.email;
-    var password = req.body.password;
+    const {nome, email, password} = req.body
+
+    if (nome && email && password) {        
+        var utilizador = await Associados.findOne({where: { email_user: email}}).then(function(data){
+        return data;
+        }).catch(error =>{
+        console.log("Erro: "+error);
+        return error;
+        })
+    
+     if (utilizador){
+        const isMatch = bcrypt.compareSync(password, utilizador.password);
+            if (email === utilizador.email_user && isMatch) {
+                
+            let token = jwt.sign({email_user: req.body.email}, config.jwtSecret,
+            {expiresIn: '1h' //expira em 1 hora
+            });
+            res.json({success: true, message:' Autenticação realizada com sucesso!', token: token, data: data});
+        }
+        else {
+        res.status.json({success: false, message: 'Erro no processo de autenticação. Tente de novo mais tarde.'});
+        }
     }
-    var utilizador = await Associados.findOne({where: { email_user: email}}).then(function(data){
-    return data;
-    }).catch(error =>{
-    console.log("Erro: "+error);
-    return error;
-    })
-    if (password === null || typeof password === "undefined") {
-        res.json({
-        success: false,
-        message: 'Campos em Branco'
-    });
-   }
-   else {
-    if (req.body.email && req.body.password && utilizador) {
-    const isMatch = bcrypt.compareSync(password, utilizador.password);
-    if (req.body.email === utilizador.email_user && isMatch) {
-    let token = jwt.sign({email_user: req.body.email}, config.jwtSecret,
-    {expiresIn: '1h' //expira em 1 hora
-    });
-    res.json({success: true, message:' Autenticação realizada com sucesso!', token: token, data: data});
-    } else {
-    res.status.json({success: false, message: 'Dados de autenticação inválidos.'});
+    else {
+     res.status.json({success: false, message: 'Dados de autenticação inválidos.'});
+     }
+
     }
-    } else {
-    res.status.json({success: false, message: 'Erro no processo de autenticação. Tente de novo mais tarde.'});
+    else {
+        res.status.json({success: false, message: 'Campos em branco.'});
     }
-}} 
+} 
 module.exports = controllers;
