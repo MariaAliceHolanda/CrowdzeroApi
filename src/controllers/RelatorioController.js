d7e21ff0d072cbbc6039c2c470a61b4827188632
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-28 20:27:06 +0100

    Update RelatorioController.js

--- src/controllers/RelatorioController.js
+++ src/controllers/RelatorioController.js
@@ -2,13 +2,5 @@ var Relatorio = require('../model/relatórios');
 var sequelize = require('../model/database');
 const controller = {}
 sequelize.sync()
-controller.teste = (req,res) => {
-    const data = {
-    name: "Nuno Costa",
-    age: 42,
-    city: 'Viseu'
-    }
-    console.log("Envio de dados do Controlador EMPLOYEE.");
-    res.json(data);
-};
+
 module.exports = controller
