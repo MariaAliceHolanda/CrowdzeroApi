bbdfd22859813c814f3faed127ed8324bacf7b35
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-29 18:25:44 +0100

    Update ReporteController.js

--- src/controllers/ReporteController.js
+++ src/controllers/ReporteController.js
@@ -22,21 +22,6 @@ controller.create = async (req,res) => {
             AssociadoId: associadoId,
         })
 
-        let qtde_incrementar
-        if (nivelReporte == 0){
-            qtde_incrementar = 'qtd_reporte_baixo'
-        }else if (nivelReporte == 1){
-            qtde_incrementar = 'qtd_reporte_medio'
-        }else if (nivelReporte == 2){
-            qtde_incrementar = 'qtde_reporte_alto'
-        }
-
-        var local = await Locais.findOne({
-                where: {id: localId}
-        })
-
-        await local.increment(qtde_incrementar, {by: 1})
-
         // Atualiza Estado do Local
         const query = `SELECT count(*) FROM "Reportes" where "Reportes"."LocaiId" = ${localId} AND DATE_PART('hour', now()::time - "createdAt"::time) * 60 +
         DATE_PART('minute', now()::time - "createdAt"::time) <= 60 AND nivel_reporte = 1;`
@@ -73,6 +58,20 @@ controller.create = async (req,res) => {
         return error;
         })
 
+        let qtde_incrementar
+        if (nivelReporte == 0){
+            qtde_incrementar = 'qtd_reporte_baixo'
+        }else if (nivelReporte == 1){
+            qtde_incrementar = 'qtd_reporte_medio'
+        }else if (nivelReporte == 2){
+            qtde_incrementar = 'qtde_reporte_alto'
+        }
+
+        var local = await Locais.findOne({
+                where: {id: localId}
+        })
+
+        await local.increment(qtde_incrementar, {by: 1})
 
         return res.status(200).json(data)
         
@@ -116,110 +115,7 @@ controller.UpdatePontuacao = async (req,res) => {
     res.json({success:true, data:dados, message:"Updated successful"});
 }
 
-function calculaEstadoLocal(id) {
-    
-    try {
-        
-        const query = `SELECT count(*) FROM "Reportes" where "Reportes"."LocaiId" = ${id} AND DATE_PART('hour', now()::time - "createdAt"::time) * 60 +
-        DATE_PART('minute', now()::time - "createdAt"::time) <= 60 AND nivel_reporte = 1;`
-        const query2 = `SELECT count(*) FROM "Reportes" where "Reportes"."LocaiId" = ${id} AND DATE_PART('hour', now()::time - "createdAt"::time) * 60 +
-        DATE_PART('minute', now()::time - "createdAt"::time) <= 60 AND nivel_reporte = 2;`
-        const query3  = `SELECT count(*) FROM "Reportes" where "Reportes"."LocaiId" = ${id} AND DATE_PART('hour', now()::time - "createdAt"::time) * 60 +
-        DATE_PART('minute', now()::time - "createdAt"::time) <= 60 AND nivel_reporte = 3;`
-        const reporteBaixo = await sequelize.query(query,{ type: QueryTypes.SELECT });
-        const reporteMedio = await sequelize.query(query2,{ type: QueryTypes.SELECT });
-        const reporteAlto = await sequelize.query(query3,{ type: QueryTypes.SELECT });
 
-        var estado = 0
-        if(reporteBaixo >= reporteMedio && reporteBaixo > reporteAlto)
-           estado = 1
-        else if(reporteMedio >= reporteBaixo && reporteMedio > reporteAlto)
-           estado = 2
-        else if(reporteAlto >= reporteMedio && reporteAlto > reporteBaixo)
-           estado = 3
-        
-
-        const data = await Locais.update({
-          //qtd_reporte_baixo: reporteBaixo,
-          //qtd_reporte_medio: reporteMedio,
-          //qtd_reporte_alto: reporteAlto,
-          estado_local: estado
-        },
-        {
-        where: { id: id}
-        })
-        .then( function(data){
-        return data;
-        })
-        .catch(error => {
-        return error;
-        })
-
-        /*const dados = {
-            baixo: reporteBaixo,
-            medio:reporteMedio,
-            alto: reporteAlto,
-            estadoLocal: estado
-        }*/
-
-        return estado;
-    } catch (error) {
-        return 0;
-    }
-};
-
-
-// OBTER DADOS DE REPORTE PARA GRÁFICO
-controller.getReportes = async (req, res) => {
-    // Dados
-    const {filtro, id} = req.query
-
-    if (id){
-        var query = `SELECT id, nome_local AS local, qtde_reporte_alto as alta,
-        qtde_reporte_alto AS média, qtd_reporte_baixo AS baixa, qtd_reporte_baixo + qtde_reporte_alto + qtde_reporte_alto AS QuantidadeReportes
-        FROM "Locais" WHERE "Locais"."InstituiçõeId"=${id}
-        ORDER BY QuantidadeReportes DESC
-        LIMIT 7`
-
-        console.log(req.query)
-
-        if (filtro){
-            var filter = ``
-            if(filtro ==1){
-                filter = `<= now() - INTERVAL '7 DAYS`
-            }else if(filtro==2){
-                filter = `<= now() - INTERVAL '30 DAYS`
-            }else if(filtro==3){
-                filter = `<= now() - INTERVAL '30 DAYS`
-                console.log(filtro)
-            }
-
-
-            query = `
-            SELECT DISTINCT "Locais".id, "Locais"."nome_local" AS "local", "Locais"."qtde_reporte_alto" AS alto,
-            "Locais"."qtd_reporte_medio" AS média, "Locais"."qtd_reporte_baixo" AS baixa, 
-            MAX("Reportes"."createdAt") as "data_ultimo_reporte"
-            FROM "Reportes"
-            INNER JOIN
-            "Locais"
-            ON "Locais".id="Reportes"."LocaiId"
-            WHERE "data_ultimo_reporte" = '2021-06-28'
-            GROUP BY "Locais"."nome_local", "Locais".id
-            LIMIT 7
-            `
-        }
-        
-        var data = await sequelize.query(query, {type: QueryTypes.SELECT})
-        .then(function(data){
-            return data
-        }).catch(e =>{
-            return e
-        })
-        res.json({success: true, message: 'Dados obtidos com sucesso.', data: data})
-    }else{
-        res.json({success: false, message: 'ID não fornecido.'})
-    }
-}
 module.exports = controller
 
 /**     SELECT id, nome_local AS local, qtde_reporte_alto as alta,
