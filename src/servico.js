96c4ce1a5dadcb8d89cd5bc5bace6863b84c588e
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-30 16:32:03 +0100

    Create servico.js

---
+++ src/servico.js
@@ -0,0 +1,14 @@
+const cron = require('node-cron');
+function verificaAtualizacaoReportes(){
+    console.log('funcao cron rodando')
+    
+    // seta o estado a 0 dos locais que não tem reporte a mais de uma hora
+    // essa tarefa é executada a cada hora no sistema
+    const data =  sequelize.query(`UPDATE public."Locais"
+	SET estado_local=0
+	WHERE DATE_PART('hour', now()::time - "ultimo_reporte"::time) * 60 +
+        DATE_PART('minute', now()::time - "ultimo_reporte"::time) > 60;`,{ type: QueryTypes.SELECT });
+
+}
+
+module.exports = cron.schedule('*/5 * * * * ', verificaAtualizacaoReportes)
