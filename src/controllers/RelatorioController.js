2bc7e30f37c4aa2a3f39eb29155cf929f49bb567
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-30 15:10:06 +0100

    Update RelatorioController.js

--- src/controllers/RelatorioController.js
+++ src/controllers/RelatorioController.js
@@ -2,5 +2,24 @@ var Relatorio = require('../model/relatórios');
 var sequelize = require('../model/database');
 const controller = {}
 sequelize.sync()
-
+controller.create = async (req,res) => {
+    // data
+    const {IDLOCAL, IDGESTOR, TIPOALERTA, HORAALERTA }= req.body;
+    // create
+    const data = await Relatorio.create({
+        LocaiId: IDLOCAL,
+        gestoreId: IDGESTOR,
+        TipoAlerta: TIPOALERTA,
+        HORAALERTA: now()
+    })
+    .then(function(data){
+    return data;
+    })
+    .catch(error =>{
+    console.log("Erro: "+error)
+    return error;
+    })
+    // return res
+    res.status(200).json({ success: true,message:"Alerta Registado",data: data});
+}
 module.exports = controller
