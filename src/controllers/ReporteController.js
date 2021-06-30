a3d4fb06e9ba362f22ab54cceafc25ea80c936c3
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-06-30 11:26:40 +0100

    Update ReporteController.js

--- src/controllers/ReporteController.js
+++ src/controllers/ReporteController.js
@@ -91,7 +91,9 @@ controller.create = async (req,res) => {
 
         // Atualiza Estado Instituição do local reportado
         const instituicao =  await sequelize.query(`SELECT "InstituiçõeId" FROM public."Locais" where id = ${localId};`,{ type: QueryTypes.SELECT });
-        console.log(instituicao)
+       
+        const instituicaoID = instituicao[0].InstituiçõeId
+        console.log(instituicaoID)
 
         // Criação de Alertas
         
