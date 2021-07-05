925320318fcc3e2631a1b7c2ee336f475bc6697f
Maria Holanda <maria.alice.holanda10@gmail.com>
2021-07-05 12:27:01 +0100

    Update ReporteController.js

--- src/controllers/ReporteController.js
+++ src/controllers/ReporteController.js
@@ -48,7 +48,7 @@ controller.create = async (req,res) => {
         DATE_PART('minute', now()::time - "createdAt"::time) > 0 AND DATE_PART('hour', now()::time - "createdAt"::time) * 60 +
         DATE_PART('minute', now()::time - "createdAt"::time) <= 60 AND nivel_reporte = 2;`
         const query3  = `SELECT count(*) FROM "Reportes" where "Reportes"."LocaiId" = ${localId} AND DATE_PART('hour', now()::time - "createdAt"::time) * 60 +
-        DATE_PART('minute', now()::time - "createdAt"::time) > 0 AND DATE_PART('hour', now()::time - "createdAt"::time) * 60 +
+        DATE_PART('minute', now()::time - "createdAt"::time) >= 0 AND DATE_PART('hour', now()::time - "createdAt"::time) * 60 +
         DATE_PART('minute', now()::time - "createdAt"::time) <= 60 AND nivel_reporte = 3;`
         const reporteBaixo = await sequelize.query(query,{ type: QueryTypes.SELECT });
         const reporteMedio = await sequelize.query(query2,{ type: QueryTypes.SELECT });
